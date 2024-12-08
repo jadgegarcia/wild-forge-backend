0250000b15a158ae37591ce1ad26410bc5e89a45
jadgegarcia <joshuajadge.garcia@cit.edu>
2024-12-08 18:42:30 +0800

    -Updated settings

--- backend/wildforge/settings.py
+++ backend/wildforge/settings.py
@@ -16,7 +16,7 @@ from dotenv import load_dotenv # type: ignore
 import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent
+BASE_DIR = Path(__file__).resolve().parent.parent
 API_REPO_DIR = BASE_DIR.parent
 
 
