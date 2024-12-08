0c0f1f2386a7952f19e71b35c614b3b4879d4cad
jadgegarcia <joshuajadge.garcia@cit.edu>
2024-12-08 22:27:14 +0800

    Updated user model

--- backend/api/models/User.py
+++ backend/api/models/User.py
@@ -1,12 +1,32 @@
 from django.db import models
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import AbstractUser, BaseUserManager
+
+class CustomUserManager(BaseUserManager):
+    def create_user(self, email, password=None, **extra_fields):
+        if not email:
+            raise ValueError("The Email field must be set")
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError('Superuser must have is_staff=True.')
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError('Superuser must have is_superuser=True.')
+
+        return self.create_user(email, password, **extra_fields)
 
 class User(AbstractUser):
     first_name = models.CharField(max_length=100)
     last_name = models.CharField(max_length=100)
     email = models.EmailField(unique=True)
-    password = models.CharField(max_length=255)
-
+    
     ADMIN = 0
     MODERATOR = 1
     BASIC = 2
@@ -17,12 +37,12 @@ class User(AbstractUser):
     )
     role = models.PositiveSmallIntegerField(choices=role_choices, null=True, blank=True)
 
-    date_created = models.DateTimeField(auto_now=True)
+    date_created = models.DateTimeField(auto_now_add=True)
     date_updated = models.DateTimeField(auto_now=True)
 
     # Disable username field
     username = None
-    
     USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = ['first_name', 'last_name', 'password']
-    
+    REQUIRED_FIELDS = ['first_name', 'last_name']
+
+    objects = CustomUserManager()  # Use custom manager
