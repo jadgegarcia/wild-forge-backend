96a46148ed16b68ef01dd90bb63bd5b9d67fc485
jadgegarcia <joshuajadge.garcia@cit.edu>
2024-12-08 20:22:09 +0800

    -Updated build command

--- build.sh
+++ build.sh
@@ -10,6 +10,7 @@ python backend/manage.py migrate
 if [[ $CREATE_SUPERUSER ]]
 then
     python backend/manage.py createsuperuser --no-input \
+        --username "$DJANGO_SUPERUSER_USERNAME" \
         --email "$DJANGO_SUPERUSER_EMAIL" \
         --password "$DJANGO_SUPERUSER_PASSWORD" \
         --first_name "Admin" --last_name "User"
