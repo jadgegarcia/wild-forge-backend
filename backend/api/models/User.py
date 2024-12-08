05f25fac0663badb92c54232c1da0397d68d10c5
jadgegarcia <joshuajadge.garcia@cit.edu>
2024-12-08 22:44:31 +0800

    Updating build commands

--- backend/api/models/User.py
+++ backend/api/models/User.py
@@ -1,5 +1,28 @@
 from django.db import models
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import AbstractUser, BaseUserManager
+
+class UserManager(BaseUserManager):
+    def create_user(self, email, password=None, **extra_fields):
+        """Create and return a regular user."""
+        if not email:
+            raise ValueError("The Email field must be set")
+        email = self.normalize_email(email)
+        extra_fields.setdefault('is_active', True)
+        return self.model(email=email, **extra_fields)
+    
+    def create_superuser(self, email, password=None, **extra_fields):
+        """Create and return a superuser."""
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+        if not email:
+            raise ValueError("The Email field must be set for superuser.")
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError("Superuser must have is_staff=True.")
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError("Superuser must have is_superuser=True.")
+        
+        return self.create_user(email, password, **extra_fields)
+
 
 class User(AbstractUser):
     first_name = models.CharField(max_length=100)
@@ -17,12 +40,17 @@ class User(AbstractUser):
     )
     role = models.PositiveSmallIntegerField(choices=role_choices, null=True, blank=True)
 
-    date_created = models.DateTimeField(auto_now=True)
+    date_created = models.DateTimeField(auto_now_add=True)
     date_updated = models.DateTimeField(auto_now=True)
 
     # Disable username field
     username = None
     
     USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = ['first_name', 'last_name', 'password']
-    
+    REQUIRED_FIELDS = ['first_name', 'last_name']
+
+    # Attach the custom manager
+    objects = UserManager()
+
+    def __str__(self):
+        return self.email
