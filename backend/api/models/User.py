3d89e7593549a01c59947dfe08bb3058bca3c0c9
jadgegarcia <joshuajadge.garcia@cit.edu>
2024-12-08 23:12:13 +0800

    Updated user model

--- backend/api/models/User.py
+++ backend/api/models/User.py
@@ -1,35 +1,32 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser, BaseUserManager
 
-class UserManager(BaseUserManager):
+class CustomUserManager(BaseUserManager):
     def create_user(self, email, password=None, **extra_fields):
-        """Create and return a regular user."""
         if not email:
             raise ValueError("The Email field must be set")
         email = self.normalize_email(email)
-        extra_fields.setdefault('is_active', True)
-        return self.model(email=email, **extra_fields)
-    
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
     def create_superuser(self, email, password=None, **extra_fields):
-        """Create and return a superuser."""
         extra_fields.setdefault('is_staff', True)
         extra_fields.setdefault('is_superuser', True)
-        if not email:
-            raise ValueError("The Email field must be set for superuser.")
-        if extra_fields.get('is_staff') is not True:
-            raise ValueError("Superuser must have is_staff=True.")
-        if extra_fields.get('is_superuser') is not True:
-            raise ValueError("Superuser must have is_superuser=True.")
-        
-        return self.create_user(email, password, **extra_fields)
 
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError('Superuser must have is_staff=True.')
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError('Superuser must have is_superuser=True.')
+
+        return self.create_user(email, password, **extra_fields)
 
 class User(AbstractUser):
     first_name = models.CharField(max_length=100)
     last_name = models.CharField(max_length=100)
     email = models.EmailField(unique=True)
-    password = models.CharField(max_length=255)
-
+    
     ADMIN = 0
     MODERATOR = 1
     BASIC = 2
@@ -45,12 +42,7 @@ class User(AbstractUser):
 
     # Disable username field
     username = None
-    
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = ['first_name', 'last_name']
 
-    # Attach the custom manager
-    objects = UserManager()
-
-    def __str__(self):
-        return self.email
+    objects = CustomUserManager()  # Use custom manager
