bd63f108e005890011cd92d57c63ee64eeffab61
jadgegarcia <joshuajadge.garcia@cit.edu>
2024-12-08 20:25:05 +0800

    Updated build commands

--- build.sh
+++ build.sh
@@ -9,9 +9,19 @@ python backend/manage.py migrate
 
 if [[ $CREATE_SUPERUSER ]]
 then
-    python backend/manage.py createsuperuser --no-input \
-        --username "$DJANGO_SUPERUSER_USERNAME" \
-        --email "$DJANGO_SUPERUSER_EMAIL" \
-        --password "$DJANGO_SUPERUSER_PASSWORD" \
-        --first_name "Admin" --last_name "User"
+    python backend/manage.py shell <<EOF
+from django.contrib.auth import get_user_model
+from django.core.management import call_command
+
+User = get_user_model()
+
+# Create the superuser
+User.objects.create_superuser(
+    username='$DJANGO_SUPERUSER_USERNAME',
+    email='$DJANGO_SUPERUSER_EMAIL',
+    password='$DJANGO_SUPERUSER_PASSWORD',
+    first_name='Admin',  # You can also set this via environment variable
+    last_name='User'     # You can also set this via environment variable
+)
+EOF
 fi
